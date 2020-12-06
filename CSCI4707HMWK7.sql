519e0a58f8fbf3c7caaebb264033a4d6486e9e7a
nowot005 <nowot005@umn.edu>
2020-12-05 22:34:44 -0600

    version 2.0

--- CSCI4707HMWK7.sql
+++ CSCI4707HMWK7.sql
@@ -1,21 +1,30 @@
---with SelectedAirports as (
-	--SELECT id
-	--FROM airport
-	--WHERE altitude > 10000
---), SelectedId1 as(
-	--SELECT route.airline_id
-	--FROM SelectedAirports
-	--INNER JOIN route
-	--ON route.source_airport_id = SelectedAirports.id
-	--AND route.destination_airport_id = SelectedAirports.id
-	--GROUP BY airline_id
---), Solution as (
-	--SELECT airline.name
-	--FROM CombineIds
-	--INNER JOIN airline
-	--ON airline.id = CombineIds.airline_id
---)
---SELECT * FROM Solution;
+with Airports as (
+	SELECT id
+	FROM airport
+	WHERE altitude > 10000
+), Departed as(
+	SELECT route.airline_id
+	FROM SelectedAirports
+	INNER JOIN route
+	ON route.source_airport_id = Airports.id
+), Arrive as(
+	SELECT route.airline_id
+	FROM SelectedAirports
+	INNER JOIN route
+	ON route.destination_airport_id = Airports.id
+), Combine as (
+	SELECT Departed.source_airport_id
+	FROM Departed
+	INNER JOIN Arrive
+	ON departed.source_airport_id  = arrive.source_airport_id
+	GROUP BY source_airport_id
+Solution as (
+	SELECT airline.name
+	FROM Combine
+	INNER JOIN airline
+	ON airline.id = Combine.airline_id
+)
+SELECT * FROM Solution;
 
 --PROBLEM 2
 
@@ -49,3 +58,25 @@ with Minneapolis as (
 	INNER JOIN MN_Athens_Stopover
 	ON airport.id = MN_Athens_Stopover.destination_airport_id
 ) SELECT * FROM Solution;
+
+--Problem 3
+with Minneapolis as (
+	SELECT id FROM airport
+	WHERE round(latitude) = 45
+	AND round(longitude) = -93
+), MN_Routes as (
+	SELECT destination_airport_id
+	FROM Minneapolis
+	INNER JOIN route
+	ON route.source_airport_id = Minneapolis.id
+), MN_Airlines as (
+	SELECT airline.name
+	FROM MN_Routes
+	INNER JOIN airline
+	ON airline.id = MN_Routes.destination_airport_id
+), Solution as (
+	SELECT * FROM MN_Airline
+	WHERE airline.name not like 'ALASKA'
+	GROUP BY airline.name
+) SELECT * FROM Solution;
+
